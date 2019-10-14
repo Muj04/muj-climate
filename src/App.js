143fdb97b48a634b669a86cc82004b6da63b6019
Mujahid Salie <mujahid.salie@gmail.com>
2019-10-14 15:22:46 +0200

    Start with create-react-app

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ e.preventDefault();
      const city = e.target.elements.city.value;
 
 const country = e.target.elements.country.value;
- const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${"6e9c4d44e0cc64f81bcffccf748d4601"}&units=metric`);
+ const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${"6e9c4d44e0cc64f81bcffccf748d4601"}&units=metric`);
   
   const response = await api_call.json();
 
