f1259419ae292cb7256317bd37422c36c76671e9
Mujahid Salie <mujahid.salie@gmail.com>
2019-10-31 14:47:31 +0200

    Start with create-react-app

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,7 +3,8 @@ const Titles = (props) =>{
   return (
     <div>
       <h1 class= "App">Weather App </h1>
-      <p> Helps you find weather conditions in cities... </p>
+      <p> Helps you find weather conditions in cities...<br/>
+      enter the countries abreviation e.g. <b>US, ZA, CA..</b> </p>
     </div>
   )
 }
